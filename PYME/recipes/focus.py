ce7e958b5cbcbb76a694b88b7a03155814a5e0bd
Barentine <andrew.barentine@yale.edu>
2021-03-26 14:44:44 -0400

    use interpolation instead of fitting to get max focus

--- PYME/recipes/focus.py
+++ PYME/recipes/focus.py
@@ -8,70 +8,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class GaussFitter1D(object):
-    """
-    1D gaussian fitter for use with focus locks which either have line-cameras, or whose frames are summed alone one
-    direction to create a line profile, the peak position of which indicates the current focal position.
-    """
-    def __init__(self, maxfev=200, min_amp=0, max_sigma=np.finfo(float).max):
-        """
-        Parameters
-        ----------
-        maxfev: int
-            see scipy.optimize.leastsq argument by the same name
-        min_amp : float
-            minimum fit result amplitude which we are willing to accept as a
-            successful fit.
-        max_sigma : float
-            maximum fit result sigma which we are willing to accept as a
-            successful fit.
-        """
-        self.maxfev = maxfev
-        self._min_amp = min_amp
-        self._max_sigma = max_sigma
-
-    def _model_function(self, parameters, position):
-        """
-        1D gaussian
-        Parameters
-        ----------
-        parameters : tuple
-            fit model parameters
-        distance : ndarray
-            1D position array [pixel]
-        Returns
-        -------
-        model : ndarray
-        """
-        amplitude, center, sigma, bx, b = parameters
-        return amplitude * np.exp(-((position - center) ** 2) / (2 * sigma ** 2)) + bx * position + b
-
-    def _error_function(self, parameters, position, data):
-        """
-        """
-        return data - self._model_function(parameters, position)
-
-    def _calc_guess(self, position, data):
-        offset = data.min()
-        p95 = np.percentile(data, 95)
-        amplitude = p95 - offset
-        max_ind = np.argmin(np.abs(data - p95))
-        fwhm = np.sum(data > offset + 0.5 * amplitude)
-        # amplitude, center, sigma, bx, b = parameters
-        return amplitude, position[max_ind], fwhm / 2.355, 0, offset
-
-    def fit(self, position, data):
-        from scipy import optimize
-
-        guess = self._calc_guess(position, data)
-
-        (res, cov_x, infodict, mesg, res_code) = optimize.leastsq(self._error_function, guess, args=(position, data),
-                                                                 full_output=True, maxfev=self.maxfev)
-
-        success = res_code > 0 and res_code < 5 and res[0] > self._min_amp and res[2] < self._max_sigma
-        return tuple(res.astype('f')), success
-
-
 @register_module('StackSettingsAboutFocus')
 class StackSettingsAboutFocus(ModuleBase):
     """
@@ -83,9 +19,8 @@ class StackSettingsAboutFocus(ModuleBase):
     output = Output('with_stack_settings')
 
     def execute(self, namespace):
-        from scipy.ndimage import laplace
+        from scipy.interpolate import interp1d
         from PYME.Analysis.piezo_movement_correction import correct_target_positions
-        # from PYME.recipes.processing import Threshold
 
         im = namespace[self.input_stack]
         
@@ -118,23 +53,21 @@ class StackSettingsAboutFocus(ModuleBase):
             metric.append(nvar[this][I])
             z_filt.append(z[this][I])
 
-        fitter = GaussFitter1D()
-        res, success = fitter.fit(np.array(z_filt), np.array(metric))
-        if not success:
-            raise RuntimeError('Fit did not converge')
+        metric_interp = interp1d(z_filt, metric, kind='quadratic')
 
-        # zz = np.linspace(z.min(), z.max(), 100)
+        zz = np.linspace(z.min(), z.max(), 10 * len(z_filt))
+        zinterpmax = float(zz[np.argmax(metric_interp(zz))])
         # fitted = fitter._model_function(res, zz)
         # plt.plot(zz, fitted)
 
-        bottom = res[1] + self.bounds[0]
-        top = res[1] + self.bounds[1]
+        bottom = zinterpmax + self.bounds[0]
+        top = zinterpmax + self.bounds[1]
 
         mdh = MetaDataHandler.DictMDHandler({
             'StackSettings.StartPos': bottom,
             'StackSettings.EndPos': top,
-            # numpy float32 won't serialize later
-            'StackSettingsAboutFocus.Center': float(res[1])
+            # numpy floats won't serialize later
+            'StackSettingsAboutFocus.Center': zinterpmax
         })
         logger.debug('StartPos %.3f, EndPos %.3f' % (bottom, top))
         if self.step_size != 0:
